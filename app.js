54b2c64f3999d202e53139b8757f6030ad808a84
tdhood <taylordannhood@gmail.com>
2022-05-31 16:30:08 -0700

    added form

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+"use strict";
+
 console.log("Let's get this party started!");
+
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+const searchTerm = $('#giphy-search').val();
+console.log(searchTerm)
+axios.get('http://api.giphy.com/v1/gifs/', {params: {'q': searchTerm, 'api_key': API_KEY}});
+
+async function getGiphy(event){
+    event.preventDefault();
+    //const searchTerm = $('#giphy-search').val();
+    console.log('click')
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${API_KEY}`);
+
+    console.log('getGiphy resp=', response);
+    $('#giphy').html(response.data);
+}
+
+$('#search-button').on('click', getGiphy);

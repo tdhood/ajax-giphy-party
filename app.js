ac00d15ce6e10e14ffca42646ae0939993fe63f4
tdhood <taylordannhood@gmail.com>
2022-05-31 17:06:05 -0700

    times up

--- app.js
+++ app.js
@@ -3,18 +3,28 @@
 console.log("Let's get this party started!");
 
 const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
-const searchTerm = $('#giphy-search').val();
-console.log(searchTerm)
-axios.get('http://api.giphy.com/v1/gifs/', {params: {'q': searchTerm, 'api_key': API_KEY}});
+const searchTerm = $('#giphy-search');
+
+axios.get('http://api.giphy.com/v1/gifs/search', {params: {'q': searchTerm, 'api_key': API_KEY}});
 
 async function getGiphy(event){
     event.preventDefault();
     //const searchTerm = $('#giphy-search').val();
     console.log('click')
-    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${API_KEY}`);
+    
+    let searchValue = searchTerm.val();
+    console.log('val', searchValue)
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchValue}&api_key=${API_KEY}`);
 
     console.log('getGiphy resp=', response);
-    $('#giphy').html(response.data);
+    $('#giphy').html(response.data.data.title);
+
+    showGiphy();
+}
+async function showGiphy() {
+    let giphyImage = $('<giphy-image>')
+    ("giphy-image").appendTo('#giphy-area')
 }
 
-$('#search-button').on('click', getGiphy);
+
+$('form').on('submit', getGiphy);
